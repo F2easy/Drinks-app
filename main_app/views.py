45621eca2856a9c1adc431c444eca3077a59d9e0
CosmicGate <jackie.sechrist.3@gmail.com>
2024-01-19 15:54:45 -0600

    Some basic routes and Urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def drinks_index(request):
+    return render(request, 'index.html')
+
+def show_page(request):
+    return(render(request, 'show.html'))
