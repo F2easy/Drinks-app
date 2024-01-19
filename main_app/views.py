5eef00888c7550bd07902552dcf1027f34f759af
John Arocha Dev <jparocha777@gmail.com>
2024-01-19 16:59:40 -0500

    api instructions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
