7b19b714946b692d88e0bb5cfc88339b772ff46b
John Arocha Dev <jparocha777@gmail.com>
2024-01-20 11:18:27 -0500

    API info

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 import requests
 
-url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
 
-payload = {}
-headers = {}
+def home(request):
+    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response = response.json
+    
+    return render(request, 'home.html', {'response': response})
 
-response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
-
+def about(request):
+    return render(request, 'about.html')
