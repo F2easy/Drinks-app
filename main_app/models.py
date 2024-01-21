6dade2db693731f47363967154bda0c49caa8ede
CosmicGate <jackie.sechrist.3@gmail.com>
2024-01-20 20:37:49 -0600

    Migration,Url and view updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient_id = models.CharField(max_length=10)
+    ingredient_name = models.CharField(max_length=255)
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,  get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import ShoppingList
 import requests
+import os
 
 # Create views here.
+api = os.environ['APIKEY']
 
 def home(request):
     url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
@@ -10,16 +14,40 @@ def home(request):
     response = requests.request("GET", url, headers=headers, data=payload)
     response = response.json
     
+    
     return render(request, 'home.html', {'response': response})
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', )
 
 
 def drinks_index(request):
-    return render(request, 'index.html')
+    api_key = os.environ['APIKEY']
+    url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    payload = {}
+    headers = {}
+    response = requests.get(url, headers=headers, data=payload)
+    drinks = response.json()['drinks']
+
+    return render(request, 'index.html', {'drinks':drinks})
 
 def show_page(request):
     return(render(request, 'show.html'))
+
+def add_to_shopping_list(request, ingredient_name):
+    user = request.user
+    ShoppingList.objects.create(user=user, ingredient_name=ingredient_name)
+    return render(request, 'show.html')
+
+#If link in the show page or the nav bar, user should see all the ingrdients that have been added to the list 
+def ShoppingList(request):
+    return render(request, 'ShoppingList.html')
+
+def delete_from_shopping_list(request, ingredient_id):
+    shopping_list_entry =  get_object_or_404(ShoppingList, ingredient_id=ingredient_id, user=request.user)
+    shopping_list_entry.delete()
+    return redirect(request, 'ShoppingList.html')
+    
+
 
