2f54b07c62ec2172681230f19cdbb871377c71b6
John Arocha Dev <jparocha777@gmail.com>
2024-01-23 09:38:58 -0500

    Code cleanup

--- main_app/models.py
+++ main_app/models.py
@@ -7,4 +7,9 @@ class ShoppingList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient_id = models.CharField(max_length=10)
     ingredient_name = models.CharField(max_length=255)
-
+    
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'list_id': self.id})
