f190a385a566b047bb0c9b8a1239901532dd6984
John Arocha Dev <jparocha777@gmail.com>
2024-01-23 10:35:37 -0500

    Added new views and URLs git push origin John

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ShoppingList(models.Model):
+class ShoppingGuide(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     drink_name = models.CharField(max_length=30)
     drink_id = models.CharField(max_length=20)
